41b608c799424d75a4c18bfdc68f7aa1bcff7bce
Sai ram <sairammallamaplli@gmail.com>
2025-06-10 19:05:19 +0530

    add

---
+++ src/ex_01_java_basic/lab4_9table.java
@@ -0,0 +1,8 @@
+package ex_01_java_basic;
+
+public class lab4_9table {
+    public static void main(String[] args) {
+        int _a = 9;
+        System.out.println(_a);
+    }
+}
---
+++ src/ex_01_java_basic/lab5_9tablewithprintf.java
@@ -0,0 +1,18 @@
+package ex_01_java_basic;
+
+public class lab5_9tablewithprintf {
+    public static void main(String[] args) {
+        System.out.printf("9*1=9\n");
+        System.out.printf("9*2=18\n");
+        System.out.printf("9*3=27\n");
+        System.out.printf("9*4=36\n");
+        System.out.printf("9*5=45\n");
+        System.out.printf("9*6=54\n");
+        System.out.printf("9*7=63\n");
+        System.out.printf("9*8=72\n");
+        System.out.printf("9*9=81\n");
+        System.out.printf("9*10=90\n");
+    }
+
+    }
+
